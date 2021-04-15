a4b26e1e7d4cc2e35579409c8072eda2e1aabb42
nothingrealm <shafaghi1378@gmail.com>
2021-04-15 14:42:13 +0430

    Added function prototypes.

--- http_request.h
+++ http_request.h
@@ -5,6 +5,18 @@
 
 typedef void (*callback)(int status, char* body, void* arg);
 
+#define HTTP_VERSION "HTTP/1.0"
+
+#define VERB_PATH_VERSION(V, P) #V " "  #P " "  HTTP_VERSION CR
+
+int write_verb_path(const int fd, const char* verb, const char* path);
+
+int write_host(const int fd, const char* host);
+
+int write_headers(const int fd, char* headers[], const int header_len);
+
+int write_body(const int fd, const char* body);
+
 int send_request(
         int fd,
         char* host,
