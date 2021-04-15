a6aef231dc2acd812dd2c32e24a4dc16fc834a1e
nothingrealm <shafaghi1378@gmail.com>
2021-04-16 01:04:42 +0430

    Coding style due to #40

--- http_request.c
+++ http_request.c
@@ -1,64 +1,69 @@
 #include "http_request.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <common.h>
 
-int write_verb_path(const int fd, const char* verb, const char* path) {
-    return dprintf(fd, "%s %s %s\r\n", verb, path, HTTP_VERSION);
+int write_verb_path_version(const int fd, const char *verb, const char *path,
+        const char* httpv) {
+    return dprintf(fd, "%s %s %s" RN, verb, path, httpv);
 }
 
 int write_host(const int fd, const char* host) {
-    return dprintf(fd, "HOST: %s\r\n", host);
+    return dprintf(fd, "HOST: %s" RN, host);
 }
 
-int write_headers(const int fd, char* headers[], const int header_count) {
-    int i, err, sum = 0;
+int write_headers(const int fd, char *headers[], const int header_count) {
+    int i;
+    int err;
+    int sum = 0;
     for (i = 0; i < header_count; i++) {
-        err = dprintf(fd, "%s\r\n", headers[i]);
-        if (err < 0)
-            return -1;
+        err = dprintf(fd, "%s" RN, headers[i]);
+        if (err < 0) {
+            return ERR;
+        }
         sum += err;
     }
     return sum;
 }
 
-int write_body(const int fd, const char* body) {
-    int err, sum = 0;
-    err = dprintf(fd, "Content-Length: %ld\r\n\r\n", strlen(body));
-    if (err < 0)
-        return -1;
+int write_body(const int fd, const char *body, const int body_len) {
+    int err;
+    int sum = 0;
+    err = dprintf(fd, "Content-Length: %ld" RN RN, strlen(body));
+    if (err < 0) {
+        return ERR;
+    }
     sum += err;
-    err = write(fd, body, strlen(body));
-    if (err < 0)
-        return -1;
+    err = write(fd, body, body_len);
+    if (err < 0) {
+        return ERR;
+    }
     return sum + err;
 }
 
-int send_request(
-        int fd,
-        char* host,
-        char* verb,
-        char* path,
-        char* headers[],
-        int header_count,
-        char* body,
-        callback cb
-    ) {
-    int err, sum = 0;
-    err = write_verb_path(fd, verb, path);
-    if (err < 0)
-        return -1;
+int send_request(int fd, char *host, char *verb, char *path, char *headers[],
+        int header_count, char *body, int body_len, callback cb, char *httpv) {
+    int err;
+    int sum = 0;
+    err = write_verb_path_version(fd, verb, path, httpv);
+    if (err < 0) {
+        return ERR;
+    }
     sum += err;
     err = write_host(fd, host);
-    if (err < 0)
-        return -1;
+    if (err < 0) {
+        return ERR;
+    }
     sum += err;
     err = write_headers(fd, headers, header_count);
-    if (err < 0)
-        return -1;
+    if (err < 0) {
+        return ERR;
+    }
     sum += err;
-    err = write_body(fd, body);
-    if (err < 0)
-        return -1;
+    err = write_body(fd, body, body_len);
+    if (err < 0) {
+        return ERR;
+    }
     sum += err;
     return sum;
 }
--- tests/test_http_request.c
+++ tests/test_http_request.c
@@ -5,57 +5,56 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define PATH "/tmp/http_request.txt"
+#define TMP_F tempnam("/tmp/", NULL)
 
-int 
-mock_fd() {
-    return open(PATH, O_RDWR | O_CREAT, S_IRWXU);
-}
+#define MOCKFD open(TMP_F, O_RDWR | O_CREAT, S_IRWXU)
 
-int 
-clean_fd() {
-    return remove(PATH);
-}
+#define CLEANFD remove(TMP_F)
 
 void
-test_write_verb_path() {
-    int fd, len;
-    char *vpv, *response;
-    vpv = VERB_PATH_VERSION(GET, /index.html);
+test_write_verb_path_version() {
+    int fd;
+    int len;
+    char *vpv;
+    char *response;
+    vpv = "GET /index.html " HTTP_V1_1 RN;
     len = strlen(vpv);
     response = (char*) malloc(len * sizeof(char));
-    fd = mock_fd();
-    write_verb_path(fd, "GET", "/index.html");
+    fd = MOCKFD;
+    write_verb_path_version(fd, "GET", "/index.html", HTTP_V1_1);
     lseek(fd, 0, SEEK_SET);
     read(fd, response, len);
     eqstr(vpv, response);
     free(response);
-    clean_fd();
+    CLEANFD;
 }
 
 void
 test_write_host() {
-    int fd, len;
-    char* host = "google.com";
-    char *response, *expected;
-    fd = mock_fd();
+    int fd;
+    int len;
+    char *host = "google.com";
+    char *response;
+    fd = MOCKFD;
     len = write_host(fd, host);
-    notequalint(-1, len);
-    expected = (char*) malloc(len * sizeof(char));
-    sprintf(expected, "HOST: %s\r\n", host);
+    notequalint(ERR, len);
     response = (char*) malloc(len * sizeof(char));
     lseek(fd, 0, SEEK_SET);
     read(fd, response, len);
-    eqstr(expected, response);
-    clean_fd();
+    eqstr("HOST: google.com" RN, response);
+    free(response);
+    CLEANFD;
 }
 
 void
 test_write_headers() {
-    int fd, len, header_count, i;
-    char* response;
-    char* expected;
-    char* headers[] = {
+    int fd;
+    int len;
+    int header_count;
+    int i;
+    char *response;
+    char *expected;
+    char *headers[] = {
         "Accept-Language: en-US,en;q=0.5",
         "Accept-Encoding: gzip, deflate, br",
         "Referer: https://developer.mozilla.org/testpage.html",
@@ -63,9 +62,9 @@ test_write_headers() {
         "Upgrade-Insecure-Requests: 1"
     };
     header_count = 5;
-    fd = mock_fd();
+    fd = MOCKFD;
     len = write_headers(fd, headers, header_count);
-    notequalint(-1, len);
+    notequalint(ERR, len);
     lseek(fd, 0, SEEK_SET);
     response = (char*) malloc(len * sizeof(char));
     expected = (char*) malloc(len * sizeof(char));
@@ -75,30 +74,36 @@ test_write_headers() {
         strcat(expected, "\r\n");
     }
     eqstr(expected, response);
-    clean_fd();
+    free(response);
+    free(expected);
+    CLEANFD;
 }
 
 void
 test_write_body() {
-    int fd, len;
-    char* response;
-    char* expected;
-    char* body = "{\"name\": \"Dennis\", \"lastname\": \"Ritchie\"}";
-    fd = mock_fd();
-    len = write_body(fd, body);
-    notequalint(-1, len);
+    int fd;
+    int len;
+    char *response;
+    char *expected;
+    char *body = "foo baz bar cux";
+    fd = MOCKFD;
+    len = write_body(fd, body, 15);
+    notequalint(ERR, len);
     lseek(fd, 0, SEEK_SET);
     response = (char*) malloc(len * sizeof(char));
     expected = (char*) malloc(len * sizeof(char));
     sprintf(expected, "Content-Length: %ld\r\n\r\n%s", strlen(body), body);
     read(fd, response, len);
     eqstr(expected, response);
-    clean_fd();
+    free(response);
+    free(expected);
+    CLEANFD;
 }
 
 int
 main() {
-    test_write_verb_path();
+    log_setlevel(LL_DEBUG);
+    test_write_verb_path_version();
     test_write_host();
     test_write_headers();
     test_write_body();
