16106c43d98ef9efc644be56518901682475b75b
nothingrealm <shafaghi1378@gmail.com>
2021-04-15 14:36:57 +0430

    Basic implemention of send_request function

---
+++ http_request.c
@@ -0,0 +1,33 @@
+#include "http_request.h"
+#include <unistd.h>
+#include <stdio.h>
+
+int send_request(
+        int fd,
+        char* host,
+        char* verb,
+        char* path,
+        char* headers[],
+        int header_count,
+        char* body,
+        callback cb
+    ) {
+    int err, sum = 0;
+    err = write_verb_path(fd, verb, path);
+    if (err < 0)
+        return -1;
+    sum += err;
+    err = write_host(fd, host);
+    if (err < 0)
+        return -1;
+    sum += err;
+    err = write_headers(fd, headers, header_count);
+    if (err < 0)
+        return -1;
+    sum += err;
+    err = write_body(fd, body);
+    if (err < 0)
+        return -1;
+    sum += err;
+    return sum;
+}
